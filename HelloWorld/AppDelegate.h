a8acf0ed5a42d8d3f2aca7ca687e2204aa03686c
jag333 <iapp@ml2-2s-Mac-Pro.local>
2013-11-20 19:03:41 +0530

    Initial Commit

---
+++ HelloWorld/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HelloWorld
+//
+//  Created by ml2-2 on 11/20/13.
+//  Copyright (c) 2013 ml2-2. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HelloWorld/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  HelloWorld
+//
+//  Created by ml2-2 on 11/20/13.
+//  Copyright (c) 2013 ml2-2. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
